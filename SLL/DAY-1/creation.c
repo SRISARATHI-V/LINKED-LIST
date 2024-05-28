ba3da39aab25191539f0f0d2c6d48650024b3181
SRISARATHI V <158599725+SRISARATHI-V@users.noreply.github.com>
2024-05-28 14:52:11 +0530

    Create creation.c

---
+++ SLL/DAY-1/creation.c
@@ -0,0 +1,24 @@
+#include<stdio.h>
+#include<stdlib.h>
+
+struct node{
+  int data;
+  struct node *link;
+};
+int main(){
+  struct node *head = malloc(sizeof(struct node));
+  head->data=10;
+  head->link=null;
+
+  struct node *current = malloc(sizeof(struct node));
+  current->data=20;
+  current->link=null;
+  head->link=current;
+
+  current = malloc(sizeof(struct node));
+  current->data=30;
+  current->link=null;
+  head->link->link=current;
+
+  return 0;
+}
