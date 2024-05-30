3b6f3e5327b1c16eb941dd8791909ed91ab74a26
SRISARATHI V <158599725+SRISARATHI-V@users.noreply.github.com>
2024-05-30 15:54:23 +0530

    Create delete_beg.c

---
+++ DLL/DAY_3/delete_beg.c
@@ -0,0 +1,53 @@
+#include<stdio.h>
+#include<stdlb.h>
+
+struct node{
+  struct node *prev;
+  int data;
+  struct node *next;
+};
+struct node* create(struct node* head,int data){
+  struct node* temp=malloc(sizeof(struct node));
+  temp->prev=NULL;
+  temp->data=data;
+  temp->next=NULL;
+  head=temp;
+  return head;
+}
+
+struct node* insert_end(struct node* head,int data){
+   struct node* temp,*tp;
+   temp=malloc(sizeof(struct node));
+   temp->prev=NULL;
+   temp->data=data;
+   temp->next=NULL;
+   tp=head;
+   while(tp->next!=NULL){
+       tp=tp->next;
+   }
+   tp->next=temp;
+   temp->Prev=tp;
+   return head;
+}
+struct node* del_beg(struct node* head){
+  struct node* temp=head;
+  head=head->next;
+  free(temp);
+  temp=NULL;
+  head->prev=NULL;
+  return head;
+}
+  
+
+int main(){
+  struct node* head=NULL;
+  struct node* ptr;
+  int position=2;
+  head=create(head,10);
+  head=insert_end(head,20);
+  head=insert_end(head,30);
+  printf("before deletion:%d",head);
+  head=del_beg(head);
+  printf("after deletion;%d",head);
+  return head;
+}
