601e1822864108d9273220dd6b4cacd0006b9b6b
SRISARATHI V <158599725+SRISARATHI-V@users.noreply.github.com>
2024-05-30 15:44:04 +0530

    Update certain_pos.c

--- DLL/DAY_3/certain_pos.c
+++ DLL/DAY_3/certain_pos.c
@@ -1,5 +1,5 @@
 #include<stdio.h>
-#include<stdlb.h>
+#include<stdlib.h>
 
 struct node{
   struct node *prev;
@@ -15,21 +15,21 @@ struct node* create(struct node* head,int data){
   return head;
 }
 struct node* insert_beg(struct node* head,int data){
-   struct node* temp1=malloc(sizeof(struct node));
-   temp1->prev=NULL;
-   temp1->data=data;
-   temp1->next=NULL;
-   temp1->next=head;
-   head->prev=temp1;
-   head=temp1;
+   struct node* temp=malloc(sizeof(struct node));
+   temp->prev=NULL;
+   temp->data=data;
+   temp->next=NULL;
+   temp->next=head;
+   head->prev=temp;
+   head=temp;
    return head;
 }
 struct node* insert_end(struct node* head,int data){
    struct node* temp,*tp;
    temp=malloc(sizeof(struct node));
-   temp1->prev=NULL;
-   temp1->data=data;
-   temp1->next=NULL;
+   temp->prev=NULL;
+   temp->data=data;
+   temp->next=NULL;
    tp=head;
    while(tp->next!=NULL){
        tp=tp->next;
