07d667f2c1c90ebbfc9d5cb3c1b135b362fcf27c
SRISARATHI V <158599725+SRISARATHI-V@users.noreply.github.com>
2024-05-30 14:25:13 +0530

    Update certain_pos.c

--- DLL/DAY_3/certain_pos.c
+++ DLL/DAY_3/certain_pos.c
@@ -1,1 +1,17 @@
+#include<stdio.h>
+#include<stdlb.h>
+
+struct node{
+  struct node *prev;
+  int data;
+  struct node *next;
+};
+struct node* create(struct node* head,int data){
+  struct node* temp=malloc(sizeof(struct node));
+  temp->prev=NULL;
+  temp->data=data;
+  temp->next=NULL;
+  head=temp;
+  return head;
+}
 
