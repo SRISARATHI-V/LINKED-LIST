d8424c7aecfb058cbec3c0ba2d1b44ebee386319
SRISARATHI V <158599725+SRISARATHI-V@users.noreply.github.com>
2024-05-30 12:38:20 +0530

    Update creation.c

--- DLL/DAY_3/creation.c
+++ DLL/DAY_3/creation.c
@@ -1,1 +1,24 @@
+#include<stdio.h>
+#include<stdlb.h>
 
+struct node{
+  struct node *prev;
+  int data;
+  struct node *next;
+};
+
+int main(){
+  struct node *head=NULL;
+  head=create(head,10);
+  printf("%d",head->data);
+  return 0;
+}
+
+struct node* create(struct node* head,int data){
+  struct node* temp=malloc(sizeof(struct node));
+  temp->prev=NULL;
+  temp->data=data;
+  temp->next=NULL;
+  head=temp;
+  return head;
+}
