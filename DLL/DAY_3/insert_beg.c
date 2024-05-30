2b6fae00eb83043c5f3db961984bfecb25f32d15
SRISARATHI V <158599725+SRISARATHI-V@users.noreply.github.com>
2024-05-30 12:47:52 +0530

    Update insert_beg.c

--- DLL/DAY_3/insert_beg.c
+++ DLL/DAY_3/insert_beg.c
@@ -1,1 +1,40 @@
+#include<stdio.h>
+#include<stdlb.h>
 
+struct node{
+  struct node *prev;
+  int data;
+  struct node *next;
+};
+
+int main(){
+  struct node *head=NULL;
+  struct node *ptr;
+  head=create(head,10);
+  head=insert_beg(head,20);
+  ptr=head;
+  while(ptr!=NULL){
+    printf("%d",head->data);
+    ptr=ptr->next;
+  } 
+  return 0;
+}
+
+struct node* create(struct node* head,int data){
+  struct node* temp=malloc(sizeof(struct node));
+  temp->prev=NULL;
+  temp->data=data;
+  temp->next=NULL;
+  head=temp;
+  return head;
+}
+struct node* insert_beg(struct node* head,int data){
+   struct node* temp1=malloc(sizeof(struct node));
+   temp1->prev=NULL;
+   temp1->data=data;
+   temp1->next->NULL;
+   temp1->next=head;
+   head->prev=temp1;
+   head=temp1;
+   return head;
+}
