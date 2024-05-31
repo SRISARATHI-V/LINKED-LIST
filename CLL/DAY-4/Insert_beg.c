4c720733ddb6b815c3b693a517cef43a7b615269
SRISARATHI V <158599725+SRISARATHI-V@users.noreply.github.com>
2024-05-31 18:46:51 +0530

    Update Insert_beg.c

--- CLL/DAY-4/Insert_beg.c
+++ CLL/DAY-4/Insert_beg.c
@@ -5,3 +5,24 @@ struct node{
   int data;
   struct node* next;
 };
+struct node* creation(int data){
+  struct node* temp=malloc(sizeof(struct node));
+  temp->data=data;
+  temp->next=temp;
+  return temp;
+}
+struct node* insert_beg(struct node *tail, int data){
+ struct node* newp=malloc(sizeof(struct node));
+ newp->data=data;
+ newp->next=tail->next;
+ tail->next=newp;
+ return tail;
+}
+int main()
+{
+  struct node* tail;
+  tail= create(10);
+  tail= insert_beg(tail,20);
+  print(tail);
+  return 0;
+}
