b204a9157ba42584641abc8b202e5f49111aec6e
SRISARATHI V <158599725+SRISARATHI-V@users.noreply.github.com>
2024-05-31 12:17:01 +0530

    Update creation.c

--- CLL/DAY-4/creation.c
+++ CLL/DAY-4/creation.c
@@ -1,1 +1,20 @@
+#include<stdio.h>
+#include<stdlib.h>
 
+struct node {
+ int data;
+ struct node* next;
+}
+struct node* creation(int data){
+  struct node* temp=malloc(sizeof(struct node));
+  temp->data=data;
+  temp->next=temp;
+  return temp;
+}
+int main(){
+  int data=2024;
+  struct node* tail=;
+  tail=creation(data);
+  printf("%d",data);
+  return 0;
+}
